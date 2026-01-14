8efaf7f5906ec9027d55b8152cf0019fb3a3f40f
Dipesh Chand <111720814+Dipeshchand@users.noreply.github.com>
2026-01-14 16:12:54 +0530

    Update Album.jsx

--- src/page/Album.jsx
+++ src/page/Album.jsx
@@ -19,7 +19,7 @@ export default function Album() {
 
       // Get public photos of album
       const images = await fetch(
-        `http://localhost:5000/upload/album/${found._id}`
+        `https://aesthetic-backend-17tr.onrender.com/upload/album/${found._id}`
       ).then(r => r.json());
 
       setPhotos(images);
